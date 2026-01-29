d100f703b7d0194c962660289f304f0152c441c4
Shehana A. Alkushi <shehana.alkushi@gmail.com>
2026-01-29 15:24:33 +0300

    Add age and weight constraints for Donor table
    Added constraints to ensure donor age is between 18 and 65, and weight is at least 50.

--- sql/DDL/constraints.sql
+++ sql/DDL/constraints.sql
@@ -1,1 +1,12 @@
+=============================================
+-- Age group is between 18 and 65 for donors 
+=============================================
+ALTER TABLE Donor
+ADD CONSTRAINT chk_donor_age CHECK (Age BETWEEN 18 AND 65);
+
+=============================================
+-- Weight should be more than / equal to 50 
+=============================================
+ALTER TABLE Donor
+ADD CONSTRAINT chk_donor_weight CHECK (Weight >= 50);
 
